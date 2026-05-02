975c1d540d8784651e0f6b346a12ab6b4cd473a3
Vladimir <vlmirgorodskiy@yandex.ru>
2026-05-02 13:59:57 +0300

    Монтирование логов из docker контейнера в папку с проектом на ПК

--- bot/main.py
+++ bot/main.py
@@ -103,4 +103,10 @@ if __name__ == '__main__':
                retention="7 days",
                format="{time:YYYY-MM-DD HH:mm:ss} | {level: <8} | {name}:{function}:{line} - {message}",
                level="DEBUG")
+    # Для Docker
+    logger.add(
+        lambda msg: print(msg, end=""),
+        format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+        level="INFO"
+    )
     asyncio.run(main())
